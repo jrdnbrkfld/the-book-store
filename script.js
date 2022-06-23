c457b0c33bf645f9bfd8e4dd195cb731ffbcaa56
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-23 22:48:04 +0000

    changed style for cards

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function handleResponse(obj) {
     obj.items.forEach((item, index) => {
         if (index > 7) return; // limit 8 result
         let div = document.createElement('div');
-        div.className = 'card';
-        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="rounded m-4 img-fluid" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
-            <h1>${item.volumeInfo.title}</h1>
-            <p><strong>${item.volumeInfo.authors[0]}</strong></p>
-            <h8 m-4>${item.volumeInfo.description}</h8>
-            <h8 m-4>Pages:  ${item.volumeInfo.pageCount}</h8>`
+        div.className = 'books card shadow mt-4 d-flex';
+        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="m-4" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+            <h4 class="card-title">${item.volumeInfo.title}</h1>
+            <p class="card-text"><strong>${item.volumeInfo.authors}</strong></p>
+            <h8 class="m-4">${item.volumeInfo.description}</h8>
+            <h8 class="m-4">Pages:  ${item.volumeInfo.pageCount}</h8>`
 
         let container = document.querySelector('.booklist-cards');
         container.append(div);
