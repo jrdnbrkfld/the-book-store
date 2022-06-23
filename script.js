6b9cbcd3d609bb8dfdbc2e2f1477612d67662405
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-23 22:04:59 +0000

    changed font sizes for cards

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function handleResponse(obj) {
         if (index > 7) return; // limit 8 result
         let div = document.createElement('div');
         div.className = 'card';
-        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="rounded m-4 img-fluid" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
             <h1>${item.volumeInfo.title}</h1>
             <p><strong>${item.volumeInfo.authors[0]}</strong></p>
-            <h1>${item.volumeInfo.description}</h1>
-            <h1>${item.volumeInfo.pageCount}</h1>`
+            <h8 m-4>${item.volumeInfo.description}</h8>
+            <h8 m-4>Pages:  ${item.volumeInfo.pageCount}</h8>`
 
         let container = document.querySelector('.booklist-cards');
         container.append(div);
