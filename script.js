95e54ad1b8eb2d1de81701a320f429b54851ca8b
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-23 22:51:49 +0000

    cahgne height and width of thumbnail images

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function handleResponse(obj) {
         if (index > 7) return; // limit 8 result
         let div = document.createElement('div');
         div.className = 'books card shadow mt-4 d-flex';
-        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="m-4" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="m-4" height="200" width="150" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
             <h4 class="card-title">${item.volumeInfo.title}</h1>
             <p class="card-text"><strong>${item.volumeInfo.authors}</strong></p>
             <h8 class="m-4">${item.volumeInfo.description}</h8>
