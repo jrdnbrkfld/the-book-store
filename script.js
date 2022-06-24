b24f3dc62b8c9d6ab119b82b05ba5c3ad70930c0
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-24 19:10:55 +0000

    link css file and add featured section

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
 
 function handleResponse(obj) {
     obj.items.forEach((item, index) => {
-        if (index > 7) return; // limit 8 result
+        if (index > 5) return; // limit 6 result
         let div = document.createElement('div');
-        div.className = 'books card shadow mt-4 d-flex';
-        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="m-4" height="200" width="150" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
-            <h4 class="card-title">${item.volumeInfo.title}</h1>
-            <p class="card-text"><strong>${item.volumeInfo.authors}</strong></p>
-            <h8 class="m-4">${item.volumeInfo.description}</h8>
-            <h8 class="m-4">Pages:  ${item.volumeInfo.pageCount}</h8>`
+        div.className = 'books';
+        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="thumbnails" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+            <h4>${item.volumeInfo.title}</h1>
+            <p><strong>${item.volumeInfo.authors}</strong></p>
+            <h8>${item.volumeInfo.description}</h8>
+            <br>
+            <br>
+            <h8>Pages:  ${item.volumeInfo.pageCount}</h8>`
 
         let container = document.querySelector('.booklist-cards');
         container.append(div);
