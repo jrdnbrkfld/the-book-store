b028a69738e48ad347103c0acb180d53af7976dd
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-06-25 08:36:20 +0000

    finish code to inject 2 books in featured also injects 8 books into normal section

--- script.js
+++ script.js
@@ -3,35 +3,26 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
     .then(data => console.log(data));
 
 function handleResponse(obj) {
-    obj.items.forEach((item, index) => {
-        if (index > 7)
-        return;
-        let div = document.createElement('div');
-        div.className = 'books';
-        div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="thumbnails" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
-            <h4>${item.volumeInfo.title}</h1>
-            <p><strong>${item.volumeInfo.authors}</strong></p>
-            <h8>${item.volumeInfo.description}</h8>
-            <br>
-            <br>
-            <h8>Pages:  ${item.volumeInfo.pageCount}</h8>`
+    const featuredContainer = document.querySelector('.featured-books');
+    const otherContainer = document.querySelector('.other-books');
+    const otherBooks = obj.items
+    const featuredBooks = otherBooks.splice(7, 2)
+    featuredContainer.insertAdjacentHTML('beforeend', renderListHtml(featuredBooks))
+    otherContainer.insertAdjacentHTML('beforeend', renderListHtml(otherBooks))
+}
 
-        let container = document.querySelector('.booklist-cards');
-        container.append(div);
+function renderListHtml(items) {
+    return items.map(item => renderItemHtml(item)).join('')
+}
 
-        if (index < 6)
-        return;
-        let featured = document.createElement('featured');
-        featured.className = 'featured';
-        featured.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" class="thumbnails" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
-            <h4>${item.volumeInfo.title}</h1>
-            <p><strong>${item.volumeInfo.authors}</strong></p>
-            <h8>${item.volumeInfo.description}</h8>
-            <br>
-            <br>
-            <h8>Pages:  ${item.volumeInfo.pageCount}</h8>`
-
-        let section = document.querySelector('.booklist-featured');
-        section.append(featured);
-    })
+function renderItemHtml(item) {
+    return `<div class="book"><img src="${item.volumeInfo.imageLinks.thumbnail}" class="thumbnails" 
+    alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+    <div><h4>${item.volumeInfo.title}</h4>
+    <p><strong>${item.volumeInfo.authors}</strong></p>
+    <h8>${item.volumeInfo.description}</h8>
+    </div>
+    <br>
+    <h8>Pages:  ${item.volumeInfo.pageCount}</h8>
+    </div>`
 }
